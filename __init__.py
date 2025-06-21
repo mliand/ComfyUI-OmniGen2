2bfdc92e9b204ae3f6cb77f03bd821a747ba684f
Yuan-Man <68322456+Yuan-ManX@users.noreply.github.com>
2025-06-21 10:21:27 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,17 @@
+from .nodes import LoadOmniGen2Image, LoadOmniGen2Model, OmniGen2, SaveOmniGen2Image
 
+NODE_CLASS_MAPPINGS = {
+    "LoadOmniGen2Image": LoadOmniGen2Image,
+    "LoadOmniGen2Model": LoadOmniGen2Model,
+    "OmniGen2": OmniGen2,
+    "SaveOmniGen2Image": SaveOmniGen2Image,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "LoadOmniGen2Image": "Load OmniGen2 Image",
+    "LoadOmniGen2Model": "Load OmniGen2 Model",
+    "OmniGen2": "OmniGen2",
+    "SaveOmniGen2Image": "Save OmniGen2 Image",
+} 
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
