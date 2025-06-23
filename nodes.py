a5c3a96280d71e175dd0b5165a144ec3cc77f5bb
GreenLandisaLie <80653235+GreenLandisaLie@users.noreply.github.com>
2025-06-23 23:25:35 +0100

    Add files via upload

--- nodes.py
+++ nodes.py
@@ -6,19 +6,21 @@ import torch
 from torchvision.transforms.functional import to_pil_image, to_tensor
 from accelerate import Accelerator
 
-from omnigen2.pipelines.omnigen2.pipeline_omnigen2 import OmniGen2Pipeline
-from omnigen2.utils.img_util import resize_image
+from .omnigen2.pipelines.omnigen2.pipeline_omnigen2 import OmniGen2Pipeline
+from .omnigen2.utils.img_util import resize_image
+
+#from diffusers.hooks import apply_group_offloading # only exists in very recent commits of diffusers
 
 
-def load_pipeline(model_path, accelerator, weight_dtype):
+def load_pipeline(model_path, accelerator, weight_dtype, scheduler, offload_type):    
     pipeline = OmniGen2Pipeline.from_pretrained(
         model_path,
         torch_dtype=weight_dtype,
-        trust_remote_code=True,
+        #trust_remote_code=True,
     )
   
-    if args.scheduler == "dpmsolver":
-        from omnigen2.schedulers.scheduling_dpmsolver_multistep import DPMSolverMultistepScheduler
+    if scheduler == "dpmsolver":
+        from .omnigen2.schedulers.scheduling_dpmsolver_multistep import DPMSolverMultistepScheduler
         scheduler = DPMSolverMultistepScheduler(
             algorithm_type="dpmsolver++",
             solver_type="midpoint",
@@ -26,44 +28,40 @@ def load_pipeline(model_path, accelerator, weight_dtype):
             prediction_type="flow_prediction",
         )
         pipeline.scheduler = scheduler
-
-    if args.enable_sequential_cpu_offload:
+    else:
+        from .omnigen2.schedulers.scheduling_flow_match_euler_discrete import FlowMatchEulerDiscreteScheduler
+        pipeline.scheduler = FlowMatchEulerDiscreteScheduler()
+    
+    if offload_type == "sequential_cpu_offload":
         pipeline.enable_sequential_cpu_offload()
-    elif args.enable_model_cpu_offload:
+    elif offload_type == "cpu_offload":
         pipeline.enable_model_cpu_offload()
+    #elif offload_type == "group_offload":
+    #    apply_group_offloading(pipeline.transformer, onload_device=accelerator.device, offload_type="block_level", num_blocks_per_group=2, use_stream=True)
+    #    apply_group_offloading(pipeline.mllm, onload_device=accelerator.device, offload_type="block_level", num_blocks_per_group=2, use_stream=True)
+    #    apply_group_offloading(pipeline.vae, onload_device=accelerator.device, offload_type="block_level", num_blocks_per_group=2, use_stream=True)
     else:
         pipeline = pipeline.to(accelerator.device)
       
     return pipeline
 
 
-def preprocess(input_image_path: List[str] = []) -> Tuple[str, str, List[Image.Image]]:
+def preprocess(image1_path: str, image2_path: str, image3_path: str) -> List[Image.Image]:
     """Preprocess the input images."""
-  
-    # Process input images
-    input_images = None
-
-    if input_image_path:
-        input_images = []
-        if isinstance(input_image_path, str):
-            input_image_path = [input_image_path]
-
-        if len(input_image_path) == 1 and os.path.isdir(input_image_path[0]):
-            input_images = [Image.open(os.path.join(input_image_path[0], f)).convert("RGB")
-                          for f in os.listdir(input_image_path[0])]
-        else:
-            input_images = [Image.open(path).convert("RGB") for path in input_image_path]
-
-        input_images = [ImageOps.exif_transpose(img) for img in input_images]
+    
+    image_paths = [p for p in [image1_path, image2_path, image3_path] if p.strip() != ""]
+    
+    input_images = [Image.open(path).convert("RGB") for path in image_paths]
+    input_images = [ImageOps.exif_transpose(img) for img in input_images]
 
     return input_images
 
 
-def run(pipeline, input_images, width, height, num_inference_step, text_guidance_scale, image_guidance_scale, cfg_range_start, cfg_range_end, 
+def run(pipeline, input_images, seed, width, height, num_inference_step, text_guidance_scale, image_guidance_scale, cfg_range_start, cfg_range_end, 
         num_images_per_prompt, accelerator, instruction, negative_prompt):
     """Run the image generation pipeline with the given parameters."""
           
-    generator = torch.Generator(device=accelerator.device).manual_seed(args.seed)
+    generator = torch.Generator(device=accelerator.device).manual_seed(seed)
 
     results = pipeline(
         prompt=instruction,
@@ -84,7 +82,7 @@ def run(pipeline, input_images, width, height, num_inference_step, text_guidance
     return results
 
 
-def create_collage(images: List[torch.Tensor]) -> Image.Image:
+def create_collage(images: List[torch.Tensor]) -> torch.Tensor:
     """Create a horizontal collage from a list of images."""
   
     max_height = max(img.shape[-2] for img in images)
@@ -97,7 +95,7 @@ def create_collage(images: List[torch.Tensor]) -> Image.Image:
         canvas[:, :h, current_x:current_x+w] = img * 0.5 + 0.5
         current_x += w
     
-    return to_pil_image(canvas)
+    return canvas
 
 
 class LoadOmniGen2Image:
@@ -105,18 +103,30 @@ class LoadOmniGen2Image:
     def INPUT_TYPES(s):
         return {
             "required": {
-                "image_path": ("STRING", {"default": "assets/demo.png"}),
-            }
+                "image1_path": ("STRING", {"default": "assets/demo.png", "tooltip": "Required"}),
+            },
+            "optional": {
+                "image2_path": ("STRING", {"default": "", "tooltip": "Optional"}),
+                "image3_path": ("STRING", {"default": "", "tooltip": "Optional"}),
+            },
         }
 
     RETURN_TYPES = ("IMAGE",)
     RETURN_NAMES = ("image",)
-    FUNCTION = "input_image_path"
+    OUTPUT_TOOLTIPS = ("A list of PIL images. You should connect this directly to 'OmniGen2'.",)
+    FUNCTION = "input_image"
     CATEGORY = "OmniGen2"
+    DESCRIPTION = """
+This node loads images from their paths and transposes them accordingly to their EXIF data.
+ATTENTION: the output of this node is a list of PIL images. ComfyUI normally transfers images as torch.Tensor in between nodes so its highly recommended you connect the output of this node directly to 'OmniGen2'.
+"""
 
-    def input_image(self, image_path):
-        input_images = preprocess(image_path)
-        return (input_images,)
+    def input_image(self, image1_path, image2_path="", image3_path=""):
+        for p in [image1_path, image2_path, image3_path]:
+            if (p.strip() != "" or p == image1_path) and not os.path.isfile(p):
+                raise RuntimeError(f"[ERROR] File not found: {p}")
+        
+        return (preprocess(image1_path, image2_path, image3_path),)
 
 
 class LoadOmniGen2Model:
@@ -126,6 +136,13 @@ class LoadOmniGen2Model:
             "required": {
                 "model_path": ("STRING", {"default": "OmniGen2/OmniGen2"}),
                 "dtype": (["fp32", "fp16", "bf16"], {"default": "bf16"}),
+                "scheduler": (["euler", "dpmsolver"], {"default": "euler"}),
+                #"device": (["cuda", "cpu"], {"default": "cuda"}),
+                "offload_type": (
+                    #["none", "sequential_cpu_offload", "cpu_offload", "group_offload"], 
+                    ["none", "sequential_cpu_offload", "cpu_offload"], 
+                    {"default": "sequential_cpu_offload"}
+                ),
             }
         }
 
@@ -133,8 +150,8 @@ class LoadOmniGen2Model:
     RETURN_NAMES = ("pipeline",)
     FUNCTION = "load_model"
     CATEGORY = "OmniGen2"
-
-    def load_model(self, model_path, dtype):
+    
+    def load_model(self, model_path, dtype, scheduler, offload_type):
         # Initialize accelerator
         accelerator = Accelerator(mixed_precision=dtype if dtype != 'fp32' else 'no')
             
@@ -146,7 +163,7 @@ class LoadOmniGen2Model:
             weight_dtype = torch.bfloat16
     
         # Load pipeline and process inputs
-        pipeline = load_pipeline(model_path, accelerator, weight_dtype)
+        pipeline = load_pipeline(model_path, accelerator, weight_dtype, scheduler, offload_type)
         
         return (pipeline,)
 
@@ -157,8 +174,9 @@ class OmniGen2:
         return {
             "required": {
                 "pipeline": ("MODEL",),
-                "input_images": ("IMAGE",),
+                "input_images": ("IMAGE", {"tooltip": "TIP: Connect this to 'Load OmniGen2 Image' node."}),
                 "dtype": (["fp32", "fp16", "bf16"], {"default": "bf16"}),
+                "seed": ("INT", {"default": 0, "min": 0, "max": 0xffffffffffffffff}),
                 "width": ("INT", {"default": 1024}),
                 "height": ("INT", {"default": 1024}),
                 "num_inference_step": ("INT", {"default": 50}),
@@ -172,51 +190,25 @@ class OmniGen2:
             }
         }
 
-    RETURN_TYPES = ("IMAGE",)
-    RETURN_NAMES = ("results",)
+    RETURN_TYPES = ("IMAGE", "IMAGE",)
+    RETURN_NAMES = ("images", "collage",)
+    OUTPUT_IS_LIST = (True, False,)
     FUNCTION = "generate"
     CATEGORY = "OmniGen2"
 
-    def generate(self, pipeline, input_images, width, height, num_inference_step, text_guidance_scale, image_guidance_scale, cfg_range_start, cfg_range_end, 
-                      num_images_per_prompt, accelerator, instruction, negative_prompt):
+    def generate(self, pipeline, input_images, dtype, seed, width, height, num_inference_step, text_guidance_scale, image_guidance_scale, cfg_range_start, cfg_range_end, 
+                      num_images_per_prompt, instruction, negative_prompt):
 
         # Initialize accelerator
         accelerator = Accelerator(mixed_precision=dtype if dtype != 'fp32' else 'no')
                           
-        results = run(pipeline, input_images, width, height, num_inference_step, text_guidance_scale, image_guidance_scale, cfg_range_start, cfg_range_end, 
+        results = run(pipeline, input_images, seed, width, height, num_inference_step, text_guidance_scale, image_guidance_scale, cfg_range_start, cfg_range_end, 
                       num_images_per_prompt, accelerator, instruction, negative_prompt)
+                      
+        images = [to_tensor(image) * 2 - 1 for image in results.images]
+        collage = create_collage(images)
         
-        return (results,)
+        return (images, collage,)
 
 
-class SaveOmniGen2Image:
-    @classmethod
-    def INPUT_TYPES(cls):
-        return {
-            "required": {
-                "output_image_path": ("STRING", {"default": "output.png"}),
-                "results": ("IMAGE",),
-            }
-        }
 
-    RETURN_TYPES = ()
-    RETURN_NAMES = ()
-    FUNCTION = "save"
-    CATEGORY = "OmniGen2"
-
-    def save(self, output_image_path, results):
-
-        os.makedirs(os.path.dirname(output_image_path), exist_ok=True)
-    
-        if len(results.images) > 1:
-            for i, image in enumerate(results.images):
-                image_name, ext = os.path.splitext(output_image_path)
-                image.save(f"{image_name}_{i}{ext}")
-    
-        vis_images = [to_tensor(image) * 2 - 1 for image in results.images]
-        output_image = create_collage(vis_images)
-    
-        output_image.save(output_image_path)
-        print(f"Image saved to {output_image_path}")
-        
-        return ()
